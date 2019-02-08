45991cd0e22cc1a5b3fe2bb888606145b9ca0c64
Gotterdemarung <gotterdemarung@gmail.com>
2019-02-09 00:27:15 +0200

    mixed optional

--- map.go
+++ map.go
@@ -8,7 +8,7 @@ import "time"
 
 // Map applies mapping function on optional value if it presents
 func (b Bool) Map(f func(bool) bool) Bool {
-	if f == nil || b.bool == nil {
+	if f == nil || !b.IsPresent() {
 		return emptyBool
 	}
 
@@ -17,7 +17,7 @@ func (b Bool) Map(f func(bool) bool) Bool {
 
 // MapToInt applies mapping function on optional value if it presents
 func (b Bool) MapToInt(f func(bool) int) Int {
-	if f == nil || b.bool == nil {
+	if f == nil || !b.IsPresent() {
 		return emptyInt
 	}
 
@@ -26,7 +26,7 @@ func (b Bool) MapToInt(f func(bool) int) Int {
 
 // MapToString applies mapping function on optional value if it presents
 func (b Bool) MapToString(f func(bool) string) String {
-	if f == nil || b.bool == nil {
+	if f == nil || !b.IsPresent() {
 		return emptyString
 	}
 
@@ -35,7 +35,7 @@ func (b Bool) MapToString(f func(bool) string) String {
 
 // MapToFloat64 applies mapping function on optional value if it presents
 func (b Bool) MapToFloat64(f func(bool) float64) Float64 {
-	if f == nil || b.bool == nil {
+	if f == nil || !b.IsPresent() {
 		return emptyFloat64
 	}
 
@@ -48,7 +48,7 @@ func (b Bool) MapToFloat64(f func(bool) float64) Float64 {
 
 // Map applies mapping function on optional value if it presents
 func (i Int) Map(f func(int) int) Int {
-	if f == nil || i.int == nil {
+	if f == nil || !i.IsPresent() {
 		return emptyInt
 	}
 
@@ -57,7 +57,7 @@ func (i Int) Map(f func(int) int) Int {
 
 // MapToBool applies mapping function on optional value if it presents
 func (i Int) MapToBool(f func(int) bool) Bool {
-	if f == nil || i.int == nil {
+	if f == nil || !i.IsPresent() {
 		return emptyBool
 	}
 
@@ -66,7 +66,7 @@ func (i Int) MapToBool(f func(int) bool) Bool {
 
 // MapToString applies mapping function on optional value if it presents
 func (i Int) MapToString(f func(int) string) String {
-	if f == nil || i.int == nil {
+	if f == nil || !i.IsPresent() {
 		return emptyString
 	}
 
@@ -75,7 +75,7 @@ func (i Int) MapToString(f func(int) string) String {
 
 // MapToFloat64 applies mapping function on optional value if it presents
 func (i Int) MapToFloat64(f func(int) float64) Float64 {
-	if f == nil || i.int == nil {
+	if f == nil || !i.IsPresent() {
 		return emptyFloat64
 	}
 
@@ -88,7 +88,7 @@ func (i Int) MapToFloat64(f func(int) float64) Float64 {
 
 // Map applies mapping function on optional value if it presents
 func (s String) Map(f func(string) string) String {
-	if f == nil || s.string == nil {
+	if f == nil || !s.IsPresent() {
 		return emptyString
 	}
 
@@ -97,7 +97,7 @@ func (s String) Map(f func(string) string) String {
 
 // MapToBool applies mapping function on optional value if it presents
 func (s String) MapToBool(f func(string) bool) Bool {
-	if f == nil || s.string == nil {
+	if f == nil || !s.IsPresent() {
 		return emptyBool
 	}
 
@@ -106,7 +106,7 @@ func (s String) MapToBool(f func(string) bool) Bool {
 
 // MapToInt applies mapping function on optional value if it presents
 func (s String) MapToInt(f func(string) int) Int {
-	if f == nil || s.string == nil {
+	if f == nil || !s.IsPresent() {
 		return emptyInt
 	}
 
@@ -115,7 +115,7 @@ func (s String) MapToInt(f func(string) int) Int {
 
 // MapToFloat64 applies mapping function on optional value if it presents
 func (s String) MapToFloat64(f func(string) float64) Float64 {
-	if f == nil || s.string == nil {
+	if f == nil || !s.IsPresent() {
 		return emptyFloat64
 	}
 
@@ -128,7 +128,7 @@ func (s String) MapToFloat64(f func(string) float64) Float64 {
 
 // Map applies mapping function on optional value if it presents
 func (f Float64) Map(c func(float64) float64) Float64 {
-	if c == nil || f.float64 == nil {
+	if c == nil || !f.IsPresent() {
 		return emptyFloat64
 	}
 
@@ -137,7 +137,7 @@ func (f Float64) Map(c func(float64) float64) Float64 {
 
 // MapToBool applies mapping function on optional value if it presents
 func (f Float64) MapToBool(c func(float64) bool) Bool {
-	if c == nil || f.float64 == nil {
+	if c == nil || !f.IsPresent() {
 		return emptyBool
 	}
 
@@ -146,7 +146,7 @@ func (f Float64) MapToBool(c func(float64) bool) Bool {
 
 // MapToInt applies mapping function on optional value if it presents
 func (f Float64) MapToInt(c func(float64) int) Int {
-	if c == nil || f.float64 == nil {
+	if c == nil || !f.IsPresent() {
 		return emptyInt
 	}
 
@@ -155,7 +155,7 @@ func (f Float64) MapToInt(c func(float64) int) Int {
 
 // MapToString applies mapping function on optional value if it presents
 func (f Float64) MapToString(c func(float64) string) String {
-	if c == nil || f.float64 == nil {
+	if c == nil || !f.IsPresent() {
 		return emptyString
 	}
 
@@ -168,7 +168,7 @@ func (f Float64) MapToString(c func(float64) string) String {
 
 // Map applies mapping function on optional value if it presents
 func (t Time) Map(f func(time.Time) time.Time) Time {
-	if f == nil || t.time == nil {
+	if f == nil || !t.IsPresent() {
 		return emptyTime
 	}
 
@@ -181,9 +181,58 @@ func (t Time) Map(f func(time.Time) time.Time) Time {
 
 // Map applies mapping function on optional value if it presents
 func (d Duration) Map(f func(time.Duration) time.Duration) Duration {
-	if f == nil || d.duration == nil {
+	if f == nil || !d.IsPresent() {
 		return emptyDuration
 	}
 
 	return OfDuration(f(*d.duration))
+}
+
+/*
+   Mixed map functions
+*/
+
+// Map applies mapping function on optional value if it presents
+func (m Mixed) Map(f func(interface{}) interface{}) Mixed {
+	if f != nil && m.IsPresent() {
+		return OfMixed(f(m.mixed))
+	}
+
+	return emptyMixed
+}
+
+// MapToBool applies mapping function on optional value if it presents
+func (m Mixed) MapToBool(f func(interface{}) bool) Bool {
+	if f != nil && m.IsPresent() {
+		return OfBool(f(m.mixed))
+	}
+
+	return emptyBool
+}
+
+// MapToInt applies mapping function on optional value if it presents
+func (m Mixed) MapToInt(f func(interface{}) int) Int {
+	if f != nil && m.IsPresent() {
+		return OfInt(f(m.mixed))
+	}
+
+	return emptyInt
+}
+
+// MapToString applies mapping function on optional value if it presents
+func (m Mixed) MapToString(f func(interface{}) string) String {
+	if f != nil && m.IsPresent() {
+		return OfString(f(m.mixed))
+	}
+
+	return emptyString
+}
+
+// MapToFloat64 applies mapping function on optional value if it presents
+func (m Mixed) MapToFloat64(f func(interface{}) float64) Float64 {
+	if f != nil && m.IsPresent() {
+		return OfFloat64(f(m.mixed))
+	}
+
+	return emptyFloat64
 }
---
+++ mixed.go
@@ -0,0 +1,13 @@
+package optional
+
+var emptyMixed = Mixed{}
+
+// Mixed represents arbitrary interface{} optional
+type Mixed struct {
+	mixed interface{}
+}
+
+// OfMixed creates new optional interface{} holder containing provided value
+func OfMixed(src interface{}) Mixed {
+	return Mixed{mixed: src}
+}
