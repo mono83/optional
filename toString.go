8cdae6760160e84dc8d8c7a854be8b72c1aa82e8
Gotterdemarung <gotterdemarung@gmail.com>
2019-02-08 10:32:03 +0200

    to string

---
+++ toString.go
@@ -0,0 +1,40 @@
+package optional
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func (b Bool) String() string {
+	if b.bool == nil {
+		return "Optional.Empty"
+	} else if *b.bool {
+		return "Optional[true]"
+	}
+
+	return "Optional[false]"
+}
+
+func (i Int) String() string {
+	if i.int == nil {
+		return "Optional.Empty"
+	}
+
+	return "Optional[" + strconv.Itoa(*i.int) + "]"
+}
+
+func (s String) String() string {
+	if s.string == nil {
+		return "Optional.Empty"
+	}
+
+	return "Optional[" + *s.string + "]"
+}
+
+func (f Float64) String() string {
+	if f.float64 == nil {
+		return "Optional.Empty"
+	}
+
+	return fmt.Sprintf("Optional[%f]", *f.float64)
+}
